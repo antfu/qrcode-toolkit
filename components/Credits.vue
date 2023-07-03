c5b5eb6d31e8c27599a1d1bb43fc98656836520d
Anthony Fu <anthonyfu117@hotmail.com>
2023-07-03 19:00:03 +0200

    docs: update credit

--- components/Credits.vue
+++ components/Credits.vue
@@ -8,17 +8,41 @@
 
     <ul flex="~ col gap-3">
       <li ml4 list-disc>
-        <a href="https://qrbtf.com" target="_blank">QRBTF.com</a> - The original idea of AI generated QR code.
-      </li>
-      <li ml4 list-disc>
-        <a href="https://space.bilibili.com/339984/" target="_blank">@whmc76</a> - For the many great discussions and the idea of using non-square canvas.
+        <div flex="~ gap-2">
+          <VMenu placement="left-start" distance="25" skidding="-10" :delay="{ show: 0, hide: 0 }">
+            <a href="https://qrbtf.com" target="_blank">
+              QRBTF.com
+            </a>
+            <template #popper>
+              <a href="https://qrbtf.com" target="_blank" class="op100!">
+                <img src="/qrcodes/credit-qrbtf.png" h-100 w-100 rounded>
+              </a>
+            </template>
+          </VMenu>
+          - The original idea of AI generated QR code.
+        </div>
       </li>
       <li ml4 list-disc>
         <div flex="~ gap-2">
-          <VMenu>
-            <div class="link">
+          <VMenu placement="left-start" distance="25" skidding="-10" :delay="{ show: 0, hide: 0 }">
+            <a href="https://space.bilibili.com/339984/" target="_blank">
+              @whmc76
+            </a>
+            <template #popper>
+              <a href="https://space.bilibili.com/339984/" target="_blank" class="op100!">
+                <img src="/qrcodes/credit-whmc76.png" h-100 w-100 rounded>
+              </a>
+            </template>
+          </VMenu>
+          - For the many great discussions and the idea of using non-square canvas.
+        </div>
+      </li>
+      <li ml4 list-disc>
+        <div flex="~ gap-2">
+          <VMenu placement="left-start" distance="25" skidding="-10" :delay="{ show: 0, hide: 0 }">
+            <a href="https://www.xiaohongshu.com/user/profile/5be8fb806b58b745447aab0f" target="_blank">
               @代々木
-            </div>
+            </a>
             <template #popper>
               <a href="https://www.xiaohongshu.com/user/profile/5be8fb806b58b745447aab0f" target="_blank" class="op100!">
                 <img src="/qrcodes/credit-yoyogi.png" h-100 w-100 rounded>
