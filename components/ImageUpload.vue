dd85903c9ee035dd9e8f2f7d56eb34a49e3796d7
zhoulianchi <zhoulianchi@gmail.com>
2023-10-18 14:11:42 +0800

    fix: input value in ImageUpload.vue & background img resize in generator.ts

--- components/ImageUpload.vue
+++ components/ImageUpload.vue
@@ -7,7 +7,10 @@ const emit = defineEmits<{
   (event: 'update:modelValue', dataurl: string): void
 }>()
 
+const value = ref<any>()
+
 async function read(e: Event) {
+  value.value = ''
   const file = (e.target as HTMLInputElement).files?.[0]
   if (!file)
     return
@@ -27,6 +30,7 @@ async function read(e: Event) {
 <template>
   <input
     type="file" accept="image/*"
+    :value="value"
     absolute bottom-0 left-0 right-0 top-0 z-10
     max-h-full max-w-full cursor-pointer opacity-0.1
     @input="read"
